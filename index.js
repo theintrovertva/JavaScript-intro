7e868a1e60a01bc63dfb2b4a048b133dcbb5ced0
theintrovertva <theintrovertva@gmail.com>
2022-06-28 01:56:50 +0100

    commit1

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const name = 'Grace Esedeke';
+const height = '5ft 5inches';
+const country = 'Nigeria';
+
+console.log(`Name: ${name}`);
+console.log(`Height: ${height}`);
+console.log(`Country: ${country}`);
